f5fb59db41c816032331e7a42df96b015b656135
赵奇 <starry-zhao@outlook.com>
2018-12-03 20:18:58 +0800

    添加了dao接口

--- src/main/java/dao/impl/CommodityDAOImpl.java
+++ src/main/java/dao/impl/CommodityDAOImpl.java
@@ -1,13 +1,32 @@
 package dao.impl;
 
 import dao.CommodityDAO;
+import jdbc.JdbcOperator;
 import model.Commodity;
+
+import java.util.List;
 
 public class CommodityDAOImpl implements CommodityDAO {
 
+    private JdbcOperator jdbcOperator;
+
+    public CommodityDAOImpl(JdbcOperator jdbcOperator) {
+        this.jdbcOperator = jdbcOperator;
+    }
 
     @Override
     public void removeCommodity(Commodity commodity) {
 
+    }
+
+    @Override
+    public int getTotalRecord() {
+        String sql="select count(*) totalRecord from book";
+        return 0;
+    }
+
+    @Override
+    public List<Commodity> getBookList(int index, int pageSize) {
+        return null;
     }
 }
