aeea715599ffea9d447b004895af97d66d024b69
陈亮 <1210272570@qq.com>
2018-12-03 20:20:22 +0800

    ，，，

--- src/main/java/dao/CommodityDAO.java
+++ src/main/java/dao/CommodityDAO.java
@@ -1,9 +1,11 @@
 package dao;
 
+import jdbc.JdbcOperator;
 import model.Commodity;
 
 public class CommodityDAO {
 
+    private JdbcOperator jdbcOperator;
     /**
      * @param commodity 可能只有id
      *  将该商品状态设置为冻结
@@ -11,4 +13,12 @@ public class CommodityDAO {
     public void removeCommodity(Commodity commodity){
 
     }
+    public void returnCommodity(String commodityAttribute,String commodityAttributeDetails){
+
+
+
+    }
+
+
+
 }
