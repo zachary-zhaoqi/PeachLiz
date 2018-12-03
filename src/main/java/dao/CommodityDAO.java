d09c209cb0159eb7a0bc12222f0477ff46410537
赵奇 <starry-zhao@outlook.com>
2018-12-03 19:51:05 +0800

    修改model，添加了dao接口

--- src/main/java/dao/CommodityDAO.java
+++ src/main/java/dao/CommodityDAO.java
@@ -2,13 +2,13 @@ package dao;
 
 import model.Commodity;
 
-public class CommodityDAO {
+public interface CommodityDAO {
 
     /**
      * @param commodity 可能只有id
      *  将该商品状态设置为冻结
      * */
-    public void removeCommodity(Commodity commodity){
+    public void removeCommodity(Commodity commodity);
 
-    }
+
 }
---
+++ src/main/java/dao/impl/CommodityDAOImpl.java
@@ -0,0 +1,13 @@
+package dao.impl;
+
+import dao.CommodityDAO;
+import model.Commodity;
+
+public class CommodityDAOImpl implements CommodityDAO {
+
+
+    @Override
+    public void removeCommodity(Commodity commodity) {
+
+    }
+}
