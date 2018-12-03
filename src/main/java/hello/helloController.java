70325fcfbe29fb5eb1fbb076eb4a4f8f49bdaa96
赵奇 <starry-zhao@outlook.com>
2018-12-03 09:00:31 +0800

    测试spring mvc 框架可用

--- src/main/java/hello/helloController.java
+++ src/main/java/hello/helloController.java
@@ -2,7 +2,13 @@ package hello;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class helloController {
+    @RequestMapping("/")
+    public ModelAndView result(){
+        return new ModelAndView("index");
+    }
 }
