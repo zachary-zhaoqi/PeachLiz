57814509d87bd5076cb134fc47d2fe92339ed5c6
赵奇 <starry-zhao@outlook.com>
2018-12-03 09:49:17 +0800

    添加一个model

---
+++ src/main/java/model/Manager.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Manager {
+    private int idmanager;
+    private String managername;
+
+    public int getIdmanager() {
+        return idmanager;
+    }
+
+    public void setIdmanager(int idmanager) {
+        this.idmanager = idmanager;
+    }
+
+    public String getManagername() {
+        return managername;
+    }
+
+    public void setManagername(String managername) {
+        this.managername = managername;
+    }
+}
