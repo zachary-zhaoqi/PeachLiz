af162660a0ac5ff0f7b564d411cc337d4c936ad3
赵奇 <starry-zhao@outlook.com>
2018-12-06 14:07:37 +0800

    a

--- src/main/java/dao/impl/InventoryOperationDAOImpl.java
+++ src/main/java/dao/impl/InventoryOperationDAOImpl.java
@@ -3,4 +3,8 @@ package dao.impl;
 import dao.InventoryOperationDAO;
 
 public class InventoryOperationDAOImpl implements InventoryOperationDAO {
+    @Override
+    public void addOperation(int idinventory, int idoperator, int change) {
+
+    }
 }
